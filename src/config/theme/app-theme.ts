e0c6b676e04716ae2fa247f08df9de8a62ffd171
Juan Ludevid <jmludevid@hotmail.com>
2024-03-14 23:18:06 +1100

    add initial numbers on screen

---
+++ src/config/theme/app-theme.ts
@@ -0,0 +1,31 @@
+import {StyleSheet} from 'react-native';
+
+export const colors = {
+  darkGray: '#2D2D2D',
+  lightGray: '#9B9B9B',
+  orange: '#FF9427',
+
+  textPrimary: 'white',
+  textSecondary: '#666666',
+  background: '#000000',
+};
+
+export const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+  mainResult: {
+    fontSize: 70,
+    color: colors.textPrimary,
+    textAlign: 'right',
+    marginBottom: 10,
+    fontWeight: '400',
+  },
+  subResult: {
+    color: colors.textSecondary,
+    fontSize: 40,
+    textAlign: 'right',
+    fontWeight: '300',
+  },
+});
